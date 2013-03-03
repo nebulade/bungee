2b20333ce7b3405a33f1cc9abf7352e5e83d27bb
Johannes Zellner <webmaster@nebulon.de>
2013-03-02 16:10:39 -0800

    improve animations when used in delegates

--- src/quick_animation.js
+++ src/quick_animation.js
@@ -18,9 +18,11 @@ Quick.Animation = function (id, parent) {
     var elem = new Quick.Element(id, parent, "object");
     var style;
     var index = Quick._animationIndex++;
+    var dirty = true;
     var animationName = "Animation" + index;
     var keyFramesName = "AnimationKeyFrames" + index;
 
+    elem.addProperty("target", undefined);
     elem.addProperty("property", undefined);
     elem.addProperty("from", 1);
     elem.addProperty("middle", 0.5);
@@ -49,59 +51,69 @@ Quick.Animation = function (id, parent) {
     }
 
     function updateRules() {
-        var rule = "";
+        var rule1 = "";
+        var rule2 = "";
 
         if (!elem.property) {
             return;
         }
 
-        if (!style) {
-            style = document.createElement('style');
-            document.getElementsByTagName('head')[0].appendChild(style);
+        if (!Quick._style) {
+            Quick._style = document.createElement('style');
+            document.getElementsByTagName('head')[0].appendChild(Quick._style);
         }
 
-        rule += "." + animationName + " {\n";
-        rule += "   -webkit-animation: ";
-        rule += keyFramesName + " ";
-        rule += elem.duration + "ms ";
-        rule += elem.easing + " ";
-        rule += elem.delay + " ";
-        rule += elem.loops + " ";
-        rule += (elem.reverse ? "alternate" : "normal") + ";\n";
-        rule += "}\n";
+        rule1 += "." + animationName + " {\n";
+        rule1 += "   -webkit-animation: ";
+        rule1 += keyFramesName + " ";
+        rule1 += elem.duration + "ms ";
+        rule1 += elem.easing + " ";
+        rule1 += elem.delay + " ";
+        rule1 += elem.loops + " ";
+        rule1 += (elem.reverse ? "alternate" : "normal") + ";\n";
+        rule1 += "}\n";
 
-        rule += "@-webkit-keyframes " + keyFramesName + " { \n";
-        rule += "   0% { " + elem.property + ": " + elem.from + "; }";
-        rule += "   50% { " + elem.property + ": " + elem.middle + "; }";
-        rule += "   100% { " + elem.property + ": " + elem.to + "; }";
-        rule += "}";
+        rule2 += "@-webkit-keyframes " + keyFramesName + " { \n";
+        rule2 += "   0% { " + elem.property + ": " + elem.from + "; }";
+        rule2 += "   50% { " + elem.property + ": " + elem.middle + "; }";
+        rule2 += "   100% { " + elem.property + ": " + elem.to + "; }";
+        rule2 += "}";
 
-        style.innerHTML = rule;
+        // TODO also remove previous rules!
+        Quick._style.sheet.insertRule(rule1, 0);
+        Quick._style.sheet.insertRule(rule2, 0);
     }
 
-    function init() {
+    function addEventListeners() {
         elem._element = elem.target;
         if (elem._element && elem._element.element) {
             elem._element.element.addEventListener("webkitAnimationStart", animationStart, false);
             elem._element.element.addEventListener("webkitAnimationIteration", animationIteration, false);
             elem._element.element.addEventListener("webkitAnimationEnd", animationEnd, false);
         }
-        updateRules();
     }
 
-    elem.addProperty("target", undefined);
-    elem.addChanged("target", init);
-    elem.addChanged("property", updateRules);
-    elem.addChanged("from", updateRules);
-    elem.addChanged("to", updateRules);
-    elem.addChanged("middle", updateRules);
-    elem.addChanged("duration", updateRules);
-    elem.addChanged("delay", updateRules);
-    elem.addChanged("loops", updateRules);
-    elem.addChanged("reverse", updateRules);
-    elem.addChanged("easing", updateRules);
+    function markDirty() {
+        dirty = true;
+    }
+
+    elem.addChanged("target", addEventListeners);
+    elem.addChanged("property", markDirty);
+    elem.addChanged("from", markDirty);
+    elem.addChanged("to", markDirty);
+    elem.addChanged("middle", markDirty);
+    elem.addChanged("duration", markDirty);
+    elem.addChanged("delay", markDirty);
+    elem.addChanged("loops", markDirty);
+    elem.addChanged("reverse", markDirty);
+    elem.addChanged("easing", markDirty);
 
     elem.start = function () {
+        if (dirty) {
+            updateRules();
+            dirty = false;
+        }
+
         elem._element.className = animationName;
     };
 
